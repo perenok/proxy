b052ac28f11a736b104207e6627b56a04f7a8453
perenok <perenoke@gmail.com>
2022-01-04 10:10:34 +0900

    feat: 앱 v2 버전 추가

--- src/main/java/hello/proxy/trace/logtrace/LogTrace.java
+++ src/main/java/hello/proxy/trace/logtrace/LogTrace.java
@@ -5,6 +5,8 @@ import hello.proxy.trace.TraceStatus;
 public interface LogTrace {
 
     TraceStatus begin(String message);
+
     void end(TraceStatus status);
+
     void exception(TraceStatus status, Exception e);
 }
